b72d60385879293701cee1bbcd5eda85cabde7f7
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-04-30 00:46:43 +0530

    first commit

---
+++ Currency Converter/task/cconverter/cconverter.py
@@ -0,0 +1,6 @@
+# write your code here!
+coinCount = input()
+print("I have " + coinCount + " conicoins.")
+dollarAmount = int(coinCount) * 100
+print(coinCount + " conicoins cost " + str(dollarAmount) + " dollars.")
+print("I am rich! Yippee!")
---
+++ Currency Converter/task/tests.py
@@ -0,0 +1,4 @@
+from test.tests import TestStage3
+
+if __name__ == '__main__':
+    TestStage3("cconverter.cconverter").run_tests()
